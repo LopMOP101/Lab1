8202ff0bac62af054734c8df53abbf68365fbce4
LopMOP101 <49553936+LopMOP101@users.noreply.github.com>
2019-05-10 01:33:05 +0300

    Лабораторная работа 1,2 ОТПиСП

---
+++ Лаба 1.2/лаба_фигуры/лаба_фигуры/draw_tools/PolylineDrawTool.java
@@ -0,0 +1,22 @@
+﻿package draw_tools;
+
+import shapes.Point;
+import shapes.Polyline;
+
+import java.awt.event.MouseEvent;
+
+public class PolylineDrawTool extends MultidotShapeDrawTool {
+    public PolylineDrawTool(ShapesDrawer shapesDrawer) {
+        super(shapesDrawer);
+        getChooseToolBtn().setText("Ломаная");
+        getChooseToolBtn().addMouseListener(new ChooseToolBtnMouseListener(shapesDrawer){
+            public void mousePressed(MouseEvent e){
+                getShapesDrawer().setDrawTool(PolylineDrawTool.this);
+            }
+        });
+    }
+
+    protected Polyline createMultidotShape(Point point){
+        return new Polyline(point);
+    }
+}
---
+++ Лаба 1.2/лаба_фигуры/лаба_фигуры/draw_tools/SquareDrawTool.java
@@ -0,0 +1,24 @@
+﻿package draw_tools;
+
+import shapes.Point;
+import shapes.Square;
+
+import java.awt.event.MouseEvent;
+
+public class SquareDrawTool extends DrawTool {
+    public SquareDrawTool(ShapesDrawer shapesDrawer){
+        super(shapesDrawer);
+        getChooseToolBtn().setText("Квадрат");
+        getChooseToolBtn().addMouseListener(new ChooseToolBtnMouseListener(shapesDrawer){
+            public void mousePressed(MouseEvent e){
+                getShapesDrawer().setDrawTool(SquareDrawTool.this);
+            }
+        });
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e){
+        drawingShape = new Square(new Point(e.getX(), e.getY()), 1);
+        createShape(drawingShape);
+    }
+}
